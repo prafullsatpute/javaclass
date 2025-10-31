31f5d9101737af9d3bda18f8a472b8cb534b0534
prafullsatpute <satputeprafull3@gmail.com>
2025-10-31 12:23:41 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,1 +1,11 @@
-
+// array- pogram-lab1
+class AddFirstarray{
+    public static void main(String[] args) {
+        
+        int arr[]={1,2,3,4,5,6};
+        int first=arr[0];
+        int last=arr[arr.length-1];
+        int sum=first+last;
+        System.out.println("output:"+sum);
+    }
+}
